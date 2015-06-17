23a52a5398f00c90399ebfaef4d22f5cd26a5ff6
Jason Stadler <jasonstadler@gmail.com>
2015-06-17 14:43:16 -0400

    Refactored API information into a structure instead of individual constants

--- app/app-config.js
+++ app/app-config.js
@@ -1,3 +1,11 @@
+let apiKey = '4mETWN08eYj8aOvd4zixN3819mP3GUz7Ma64SxkX';
+let apiBaseUrl = 'https://api.fda.gov/food/enforcement.json';
+
+let apiInfo = {
+  key: apiKey,
+  baseUrl: apiBaseUrl,
+  url: apiBaseUrl + '?api_key=' + apiKey
+}
+
 export default angular.module('fdaFoodRecalls.config', [])
-  .constant('API_KEY', '4mETWN08eYj8aOvd4zixN3819mP3GUz7Ma64SxkX')
-  .constant('API_URL', 'https://api.fda.gov/food/enforcement.json');
+  .constant('API_INFO', apiInfo);
