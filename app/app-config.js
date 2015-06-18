1a1db81d7278aa7fb290c79111affa1f08bd0385
Jason Stadler <jasonstadler@gmail.com>
2015-06-18 12:20:20 -0400

    Added dataset period to the api info constant

--- app/app-config.js
+++ app/app-config.js
@@ -1,9 +1,13 @@
 import angular from 'angular';
-
-const apiInfo = {
-  key: '4mETWN08eYj8aOvd4zixN3819mP3GUz7Ma64SxkX',
-  url: 'https://api.fda.gov/food/enforcement.json'
-};
+import moment from 'moment';
 
 export default angular.module('fdaFoodRecalls.config', [])
-  .constant('API_INFO', apiInfo);
+  .constant('API_INFO', {
+    key: '4mETWN08eYj8aOvd4zixN3819mP3GUz7Ma64SxkX',
+    url: 'https://api.fda.gov/food/enforcement.json',
+    dataset: {
+      periodStart: moment({ year: 2012, month: 4, day: 1 }).startOf('day'),
+      periodEnd: moment().subtract(1, 'month').endOf('month')
+    }
+  })
+;
