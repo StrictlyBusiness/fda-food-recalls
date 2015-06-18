79bc831fc2cb1a3f3dd0f71e5a19091140e2c143
Sean Lynch <techniq35@gmail.com>
2015-06-18 11:13:01 -0400

    Move link function on StateMap to be a class method instead of this.link in constructor (same thing, just cleaner).  Also removed unused "d3.select(this)" calls in mouse event handlers

--- app/directives/state-map.js
+++ app/directives/state-map.js
@@ -12,50 +12,41 @@ export default class StateMap {
     this.scope = {
       data: '='
     };
-
-    this.link = (scope, element, attrs) => {
-
-      // Append path data and convert object to array for d3.data()
-      var data = Object.keys(scope.data).map(s => {
-        scope.data[s].name = s;
-        scope.data[s].path = stateShapes[s];
-        return scope.data[s];
-      });
-
-      var svgElement = element[0].querySelector('svg');
-      var tooltipElement = element[0].querySelector('.tooltip');
-
-      d3.select(svgElement).selectAll('.state')
-        .data(data).enter()
-          .append('path')
-          .attr('class', 'state')
-          .attr('d', d => d.path)
-          .style('fill', d => d3.interpolate('#FFEB3B', '#F44336')(d.count / 100))
-          .on('mouseover', function(d) {
-              d3.select(this);
-                // .style({'stroke-width': '2px', 'stroke': '#555'});
-
-              d3.select(tooltipElement)
-                .html(`${d.name} (${d.count})`)
-                .transition().duration(200).style('opacity', 1);
-          })
-          .on('mousemove', function(d) {
-            d3.select(tooltipElement)
-              .style('left', `${d3.event.pageX + 30}px`)
-              .style('top', `${d3.event.pageY - 30}px`);
-          })
-          .on('mouseout', function(d) {
-              d3.select(this);
-                // .style('stroke-width', '1px')
-                // .style('stroke', '#555');
-
-              d3.select(tooltipElement)
-                .transition().duration(200).style('opacity', 0);
-          })
-        ;
-
-    };
   }
+
+  link(scope, element, attrs) {
+    // Append path data and convert object to array for d3.data()
+    var data = Object.keys(scope.data).map(s => {
+      scope.data[s].name = s;
+      scope.data[s].path = stateShapes[s];
+      return scope.data[s];
+    });
+
+    var svgElement = element[0].querySelector('svg');
+    var tooltipElement = element[0].querySelector('.tooltip');
+
+    d3.select(svgElement).selectAll('.state')
+      .data(data).enter()
+        .append('path')
+        .attr('class', 'state')
+        .attr('d', d => d.path)
+        .style('fill', d => d3.interpolate('#FFEB3B', '#F44336')(d.count / 100))
+        .on('mouseover', function(d) {
+            d3.select(tooltipElement)
+              .html(`${d.name} (${d.count})`)
+              .transition().duration(200).style('opacity', 1);
+        })
+        .on('mousemove', function(d) {
+          d3.select(tooltipElement)
+            .style('left', `${d3.event.pageX + 30}px`)
+            .style('top', `${d3.event.pageY - 30}px`);
+        })
+        .on('mouseout', function(d) {
+            d3.select(tooltipElement)
+              .transition().duration(200).style('opacity', 0);
+        })
+      ;
+  };
 
   static directiveFactory() {
     StateMap.instance = new StateMap();
