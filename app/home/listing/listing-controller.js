6d16c2dcaeb748df35e7480d8b19669f1c5a4d4c
Jason Stadler <jasonstadler@gmail.com>
2015-06-19 10:13:12 -0400

    Added json files for recall status and recall classifications and updated list controller to use them.

--- app/home/listing/listing-controller.js
+++ app/home/listing/listing-controller.js
@@ -1,6 +1,8 @@
 import moment from 'moment';
 
 import statesDataset from '../../data/states.json!';
+import recallClassificationsDataset from '../../data/recall-classifications.json!';
+import recallStatusesDataset from '../../data/recall-statuses.json!';
 
 export default class ListingController {
 
@@ -98,33 +100,18 @@ export default class ListingController {
   //  but that violate FDA labeling or manufacturing laws. Examples include: a
   //  minor container defect and lack of English labeling in a retail food.
   getClassificationStyle(classification) {
-    if (classification === 'Class I') {
-      return 'danger';
-    }
-    if (classification === 'Class II') {
-      return 'warning';
-    }
-    if (classification === 'Class III') {
-      return 'info';
+
+    let match = recallClassificationsDataset[classification];
+    if (match) {
+      return match.style;
     }
     return 'default';
   }
+
   getClassificationDescription(classification) {
-    if (classification === 'Class I') {
-      return `Dangerous or defective products that predictably could cause
-        serious health problems or death. Examples include: food found to
-        contain botulinum toxin, food with undeclared allergens, a label
-        mix-up on a lifesaving drug, or a defective artificial heart valve.`;
-    }
-    if (classification === 'Class II') {
-      return `Products that might cause a temporary health problem, or pose
-        only a slight threat of a serious nature. Example: a drug that is
-        under-strength but that is not used to treat life-threatening situations.`;
-    }
-    if (classification === 'Class III') {
-      return `Products that are unlikely to cause any adverse health reaction,
-        but that violate FDA labeling or manufacturing laws. Examples include:
-        a minor container defect and lack of English labeling in a retail food.`;
+    let match = recallClassificationsDataset[classification];
+    if (match) {
+      return match.description;
     }
     return 'No description available';
   }
@@ -139,39 +126,18 @@ export default class ListingController {
   // Pending = Actions that have been determined to be recalls, but that remain
   //  in the process of being classified.
   getStatusStyle(status) {
-    if (status === 'Ongoing') {
-      return 'danger';
-    }
-    if (status === 'Completed') {
-      return 'success';
-    }
-    if (status === 'Terminated') {
-      return 'primary';
-    }
-    if (status === 'Pending') {
-      return 'warning';
+
+    let match = recallStatusesDataset[status];
+    if (match) {
+      return match.style;
     }
     return 'default';
   }
+
   getStatusDescription(status) {
-    if (status === 'Ongoing') {
-      return 'A recall which is currently in progress.';
-    }
-    if (status === 'Completed') {
-      return `The recall action reaches the point at which the firm has \
-        actually retrieved and impounded all outstanding product that could \
-        reasonably be expected to be recovered, or has completed all product \
-        corrections.`;
-    }
-    if (status === 'Terminated') {
-      return `FDA has determined that all reasonable efforts have been made to \
-        remove or correct the violative product in accordance with the recall \
-        strategy, and proper disposition has been made according to the degree \
-        of hazard.`;
-    }
-    if (status === 'Pending') {
-      return `Actions that have been determined to be recalls, but that remain \
-        in the process of being classified.`;
+    let match = recallStatusesDataset[status];
+    if (match) {
+      return match.description;
     }
     return 'No description available';
   }
