32d45e71e0254787261c8ada68529325a3a00bed
Sean Lynch <techniq35@gmail.com>
2015-06-19 00:13:52 -0400

    Switch from ES5 multi-line strings to ES6 template strings (with multiline support).  Fixes "no-multi-str" eslint failure

--- app/home/listing/listing-controller.js
+++ app/home/listing/listing-controller.js
@@ -111,20 +111,20 @@ export default class ListingController {
   }
   getClassificationDescription(classification) {
     if (classification === 'Class I') {
-      return 'Dangerous or defective products that predictably could cause \
-        serious health problems or death. Examples include: food found to \
-        contain botulinum toxin, food with undeclared allergens, a label \
-        mix-up on a lifesaving drug, or a defective artificial heart valve.';
+      return `Dangerous or defective products that predictably could cause
+        serious health problems or death. Examples include: food found to
+        contain botulinum toxin, food with undeclared allergens, a label
+        mix-up on a lifesaving drug, or a defective artificial heart valve.`;
     }
     if (classification === 'Class II') {
-      return 'Products that might cause a temporary health problem, or pose \
-        only a slight threat of a serious nature. Example: a drug that is \
-        under-strength but that is not used to treat life-threatening situations.';
+      return `Products that might cause a temporary health problem, or pose
+        only a slight threat of a serious nature. Example: a drug that is
+        under-strength but that is not used to treat life-threatening situations.`;
     }
     if (classification === 'Class III') {
-      return 'Products that are unlikely to cause any adverse health reaction, \
-        but that violate FDA labeling or manufacturing laws. Examples include: \
-        a minor container defect and lack of English labeling in a retail food.';
+      return `Products that are unlikely to cause any adverse health reaction,
+        but that violate FDA labeling or manufacturing laws. Examples include:
+        a minor container defect and lack of English labeling in a retail food.`;
     }
     return 'No description available';
   }
@@ -158,20 +158,20 @@ export default class ListingController {
       return 'A recall which is currently in progress.';
     }
     if (status === 'Completed') {
-      return 'The recall action reaches the point at which the firm has \
+      return `The recall action reaches the point at which the firm has \
         actually retrieved and impounded all outstanding product that could \
         reasonably be expected to be recovered, or has completed all product \
-        corrections.';
+        corrections.`;
     }
     if (status === 'Terminated') {
-      return 'FDA has determined that all reasonable efforts have been made to \
+      return `FDA has determined that all reasonable efforts have been made to \
         remove or correct the violative product in accordance with the recall \
         strategy, and proper disposition has been made according to the degree \
-        of hazard.';
+        of hazard.`;
     }
     if (status === 'Pending') {
-      return 'Actions that have been determined to be recalls, but that remain \
-        in the process of being classified.';
+      return `Actions that have been determined to be recalls, but that remain \
+        in the process of being classified.`;
     }
     return 'No description available';
   }
