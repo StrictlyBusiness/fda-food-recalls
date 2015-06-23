d4ce1c4bd17998769262522aa0d52963c2a4232d
Sean Lynch <techniq35@gmail.com>
2015-06-23 17:12:46 -0400

    Use multiline function comments

--- app/home/listing/listing-controller.js
+++ app/home/listing/listing-controller.js
@@ -89,16 +89,18 @@ export default class ListingController {
       && period.isBefore(this.API_INFO.dataset.periodEnd);
   }
 
-  // Class I = Dangerous or defective products that predictably could cause
-  //  serious health problems or death. Examples include: food found to contain
-  //  botulinum toxin, food with undeclared allergens, a label mix-up on a
-  //  lifesaving drug, or a defective artificial heart valve.
-  // Class II = Products that might cause a temporary health problem, or pose
-  //  only a slight threat of a serious nature. Example: a drug that is
-  //  under-strength but that is not used to treat life-threatening situations.
-  // Class III = Products that are unlikely to cause any adverse health reaction,
-  //  but that violate FDA labeling or manufacturing laws. Examples include: a
-  //  minor container defect and lack of English labeling in a retail food.
+  /**
+   * Class I = Dangerous or defective products that predictably could cause
+   *   serious health problems or death. Examples include: food found to contain
+   *   botulinum toxin, food with undeclared allergens, a label mix-up on a
+   *   lifesaving drug, or a defective artificial heart valve.
+   * Class II = Products that might cause a temporary health problem, or pose
+   *   only a slight threat of a serious nature. Example: a drug that is
+   *   under-strength but that is not used to treat life-threatening situations.
+   * Class III = Products that are unlikely to cause any adverse health reaction,
+   *   but that violate FDA labeling or manufacturing laws. Examples include: a
+   *   minor container defect and lack of English labeling in a retail food.
+   */
   getClassificationStyle(classification) {
 
     let match = recallClassificationsDataset[classification];
@@ -116,17 +118,18 @@ export default class ListingController {
     return 'No description available';
   }
 
-  // On-Going = A recall which is currently in progress.
-  // Completed = The recall action reaches the point at which the firm has actually
-  //  retrieved and impounded all outstanding product that could reasonably be expected
-  //  to be recovered, or has completed all product corrections.
-  // Terminated = FDA has determined that all reasonable efforts have been made to
-  //  remove or correct the violative product in accordance with the recall strategy,
-  //  and proper disposition has been made according to the degree of hazard.
-  // Pending = Actions that have been determined to be recalls, but that remain
-  //  in the process of being classified.
+  /**
+  * On-Going = A recall which is currently in progress.
+  * Completed = The recall action reaches the point at which the firm has actually
+  *   retrieved and impounded all outstanding product that could reasonably be expected
+  *   to be recovered, or has completed all product corrections.
+  * Terminated = FDA has determined that all reasonable efforts have been made to
+  *   remove or correct the violative product in accordance with the recall strategy,
+  *   and proper disposition has been made according to the degree of hazard.
+  * Pending = Actions that have been determined to be recalls, but that remain
+  *   in the process of being classified.
+  */
   getStatusStyle(status) {
-
     let match = recallStatusesDataset[status];
     if (match) {
       return match.style;
