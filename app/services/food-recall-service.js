a1ab89d26b9154c37df80ab07d47d948526ccd08
Jason Stadler <jasonstadler@gmail.com>
2015-06-29 13:50:09 -0400

    Updated the FoodRecallService to return events. Recalls are aggregated into events based on event_id. A products property on the event contains an array of products included in the recall.

--- app/services/food-recall-service.js
+++ app/services/food-recall-service.js
@@ -9,6 +9,16 @@ export default class FoodRecallService {
   constructor(openFdaService, cacheService) {
     this.openFdaService = openFdaService;
     this.cacheService = cacheService;
+
+    this.EVENT_PROPERTIES = [
+      'event_id', 'recall_number', '@epoch', 'reason_for_recall', 'status', 'classification',
+      'recalling_firm', 'city', 'state', 'country', 'report_date', 'recall_initiation_date',
+      'voluntary_mandated', 'initial_firm_notification', 'distribution_pattern'
+    ];
+
+    this.PRODUCT_PROPERTIES = [
+      '@id', 'product_quantity', 'product_type', 'product_description', 'code_info'
+    ];
   }
 
   // startDate and endDate should be moment objects
@@ -34,7 +44,7 @@ export default class FoodRecallService {
       .then(
         (results) => {
           // Process the results
-          let processedResults = this.processStatesForRecalls(results.results);
+          let processedResults = this.aggregateIntoEvents(results.results);
           // Cache the results
           this.cacheService.store(queryCacheType, queryCacheKey, processedResults);
           return processedResults;
@@ -47,24 +57,58 @@ export default class FoodRecallService {
         });
   }
 
-  // for each recall, add a states array and set the states based on processing
-  // the distribution pattern
-  processStatesForRecalls(data) {
+  // aggregate all recalls that share the same event id into a single objects
+  aggregateIntoEvents(data) {
 
-    // loop over all the recalls passed in
-    data.forEach((recall) => {
-      // add an array to store the states
-      recall.states = [];
-      // for each state, see if it matches
-      statesDataset.forEach((state) => {
-        if (this.appliesToState(state, recall)) {
-          // if it does, add it to the recall list
-          recall.states.push(state.abbr);
+    // Build a map of the events
+    let eventMap = data.reduce(
+      (events, recall) => {
+        // See if we have already created this event
+        let event = events[recall.event_id];
+        if (!event) {
+          event = {};
+          // Copy over the event specific properties
+          this.EVENT_PROPERTIES.forEach((property) => {
+            event[property] = recall[property];
+          });
+          event.distribution_states = this.getDistributionStates(event); // eslint-disable-line camelcase
+          event.products = [];
+          events[recall.event_id] = event;
         }
-      });
+
+        // Copy over the product specific proeprties
+        let product = {};
+        this.PRODUCT_PROPERTIES.forEach((property) => {
+          product[property] = recall[property];
+        });
+
+        // Add the product to the recall event
+        event.products.push(product);
+
+        return events;
+      }, {});
+
+    // Convert the object property map to an array of the values
+    let results = Object.keys(eventMap).map(key => eventMap[key]);
+
+    return results;
+  }
+
+  // Based on the events distribution_pattern, this method attempts to build
+  // an array of state names.
+  getDistributionStates(event) {
+
+    let result = [];
+
+    // for each state, see if it matches
+    statesDataset.forEach((state) => {
+      if (this.appliesToState(state, event)) {
+        // if it does, add it to the recall list
+        result.push(state.abbr);
+      }
     });
 
-    return data;
+    return result;
   }
 
   // Basic function to check if a recall applies to a state. This will be
@@ -83,8 +127,8 @@ export default class FoodRecallService {
         return true;
       }
 
-      // Check for the state abbreviation
-      let abbrExpression = new RegExp(`\\b${state.abbr}\\b`, 'gi');
+      // Check for the state abbreviation (must be upper case)
+      let abbrExpression = new RegExp(`\\b${state.abbr}\\b`, 'g');
       if (recall.distribution_pattern.match(abbrExpression) !== null) {
         return true;
       }
@@ -98,32 +142,6 @@ export default class FoodRecallService {
 
     // Assume it doesn't match
     return false;
-  }
-
-  createResultsMap() {
-
-    let map = { };
-
-    // add a map entry for unmatched entries
-    map[this.UNMATCHED] = {
-      state: {
-        name: this.UNMATCHED,
-        abbr: this.UNMATCHED
-      },
-      count: 0,
-      recalls: []
-    };
-
-    // Add a map entry for each state
-    statesDataset.forEach((state) => {
-      map[state.abbr] = {
-        state: state,
-        count: 0,
-        recalls: []
-      };
-    });
-
-    return map;
   }
 
 }
