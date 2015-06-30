f9477047aa5c04fdd03b1686aafd39aa170581b6
Jason Stadler <jasonstadler@gmail.com>
2015-06-30 09:13:54 -0400

    Removed listing view without map

--- app/home/module.js
+++ app/home/module.js
@@ -1,10 +1,8 @@
 import angular from 'angular';
 import homeRoutes from './routes';
 import mapRoutes from './map/routes';
-import listingRoutes from './listing/routes';
 
 export default angular.module('fdaFoodRecalls.home', [])
   .config(homeRoutes)
   .config(mapRoutes)
-  .config(listingRoutes)
 ;
